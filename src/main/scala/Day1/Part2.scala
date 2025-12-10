c24ba2d89544802e5de7c1aa85228d96654fac9d
Raymond Cheung <raymondcheung8@hotmail.com>
2025-12-10 23:17:51 +0000

    Implement day1 part2

---
+++ src/main/scala/Day1/Part2.scala
@@ -0,0 +1,36 @@
+package Day1
+
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.Using
+
+object Part2 extends App {
+  val input = Using(Source.fromFile("src/main/scala/Day1/input")) { source => source.mkString.split('\n').toList }.get
+
+  @tailrec
+  def getAns(input: List[String], pointer: Int = 50, count: Int = 0): Int = {
+    println(s"${input.headOption} --- $pointer --- $count")
+    input match {
+      case h :: t =>
+        val rotations = h.tail.toInt
+        h.head match {
+          case 'L' if rotations == pointer || ((rotations == 0 || rotations == 100) && pointer == 0) => getAns(t, 0, count + 1)
+          // Rotations shouldn't ever be 0
+          case 'L' if pointer == 0 && rotations != 0 && rotations < 100 => getAns(t, 100 - rotations, count)
+          case 'L' if pointer == 0 && rotations == 0                    => getAns(t, 0, count)
+          case 'L' if pointer != 0 && rotations < pointer               => getAns(t, pointer - rotations, count)
+          case 'L' if pointer == 0 && rotations > 100                   => getAns(s"L${rotations - 100}" :: t, 0, count + 1)
+          case 'L' if pointer != 0 && rotations > pointer               => getAns(s"L${rotations - pointer}" :: t, 0, count + 1)
+
+          case 'R' if rotations + pointer == 100 => getAns(t, 0, count + 1)
+          // Rotations shouldn't ever be 0
+          case 'R' if rotations + pointer == 0  => getAns(t, 0, count)
+          case 'R' if rotations + pointer > 100 => getAns(s"R${rotations + pointer - 100}" :: t, 0, count + 1)
+          case 'R' if rotations + pointer < 100 => getAns(t, rotations + pointer, count)
+        }
+      case Nil => count
+    }
+  }
+
+  println(getAns(input))
+}
