187a08d3cebbd54e8a3c7918994da283a073a581
Raymond Cheung <raymondcheung8@hotmail.com>
2025-12-16 17:26:08 +0000

    Set up day4 part1

---
+++ src/main/scala/puzzles/day4/Day4Part1.scala
@@ -0,0 +1,7 @@
+package puzzles.day4
+
+object Day4Part1 {
+  def getAns(input: Vector[Vector[Boolean]]): Int = {
+    ???
+  }
+}
--- src/test/scala/puzzles/PuzzlesTest.scala
+++ src/test/scala/puzzles/PuzzlesTest.scala
@@ -5,13 +5,14 @@ import org.scalatest.wordspec.AnyWordSpec
 import puzzles.day1.{Day1Part1, Day1Part2, Day1Part2_optimised}
 import puzzles.day2.{Day2Part1, Day2Part2}
 import puzzles.day3.{Day3Part1, Day3Part2}
+import puzzles.day4.Day4Part1
 
 class PuzzlesTest extends AnyWordSpec with Matchers {
   val answers: Map[(String, String), Long] = Utils.getAnswers
 
   "Day 1" should {
-    val day = "day1"
-    val input = Utils.getInputFromDay(day)
+    val day          = "day1"
+    val input        = Utils.getInputFromDay(day)
     val exampleInput = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82".split('\n').toList
 
     "Part 1 example" in {
@@ -37,9 +38,12 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
   }
 
   "Day 2" should {
-    val day = "day2"
-    val input = Utils.getInputFromDay(day).flatMap(_.split(',').toList)
-    val exampleInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124".split(',').toList
+    val day          = "day2"
+    val input        = Utils.getInputFromDay(day).flatMap(_.split(',').toList)
+    val exampleInput =
+      "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+        .split(',')
+        .toList
 
     "Part 1 example" in {
       Day2Part1.getAns(exampleInput) shouldEqual 1227775554L
@@ -59,8 +63,8 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
   }
 
   "Day 3" should {
-    val day = "day3"
-    val input = Utils.getInputFromDay(day).flatMap(_.split(',').toList)
+    val day          = "day3"
+    val input        = Utils.getInputFromDay(day).flatMap(_.split(',').toList)
     val exampleInput = "987654321111111\n811111111111119\n234234234234278\n818181911112111".split('\n').toList
 
     "Part 1 example" in {
@@ -77,6 +81,20 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
 
     "Part 2" in {
       Day3Part2.getAns(input) shouldEqual answers((day, "part2"))
+    }
+  }
+
+  "Day 4" should {
+    val day = "day4"
+    // Using a 2D Vector of Boolean to optimise memory usage and lookups
+    val input        = Utils.getInputFromDay(day).map(_.map(_ == '@').toVector).toVector
+    val exampleInput = "..@@.@@@@.\n@@@.@.@.@@\n@@@@@.@.@@\n@.@@@@..@.\n@@.@@@@.@@\n.@@@@@@@.@\n.@.@.@.@@@\n@.@@@.@@@@\n.@@@@@@@@.\n@.@.@@@.@."
+      .split(',')
+      .map(_.map(_ == '@').toVector)
+      .toVector
+
+    "Part 1 example" in {
+      Day4Part1.getAns(exampleInput) shouldEqual 13
     }
   }
 }
