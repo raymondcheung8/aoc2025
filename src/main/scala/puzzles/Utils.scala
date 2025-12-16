2cd8a5c96e6dad07302896434557ea3d37c3a509
Raymond Cheung <raymondcheung8@hotmail.com>
2025-12-16 14:54:27 +0000

    Store answers in file

--- src/main/scala/puzzles/Utils.scala
+++ src/main/scala/puzzles/Utils.scala
@@ -1,15 +1,20 @@
 package puzzles
 
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 import scala.io.Source
 import scala.util.Using
 
 object Utils {
-  private val resourcesPath = Paths.get("src/main/resources")
-  private val inputFileName = "input"
+  private val resourcesPath   = Paths.get("src/main/resources")
+  private val inputFileName   = "input"
+  private val answersFileName = "answers"
 
-  def getInputFromDay(day: String): List[String] =
-    Using(Source.fromFile(resourcesPath.resolve(day).resolve(inputFileName).toUri)) { source =>
-      source.mkString.split('\n').toList
-    }.get
+  private def readFile(path: Path): List[String] = Using(Source.fromFile(path.toUri)) { _.mkString.split('\n').toList }.get
+
+  def getInputFromDay(day: String): List[String] = readFile(resourcesPath.resolve(day).resolve(inputFileName))
+
+  def getAnswers: Map[(String, String), Long] =
+    readFile(resourcesPath.resolve(answersFileName))
+      .map(_.split(',') match { case Array(day, part, answer, _*) => (day, part) -> answer.trim.toLong })
+      .toMap
 }
--- src/test/scala/puzzles/PuzzlesTest.scala
+++ src/test/scala/puzzles/PuzzlesTest.scala
@@ -7,9 +7,11 @@ import puzzles.day2.{Day2Part1, Day2Part2}
 import puzzles.day3.{Day3Part1, Day3Part2}
 
 class PuzzlesTest extends AnyWordSpec with Matchers {
+  val answers: Map[(String, String), Long] = Utils.getAnswers
 
   "Day 1" should {
-    val input = Utils.getInputFromDay("day1")
+    val day = "day1"
+    val input = Utils.getInputFromDay(day)
     val exampleInput = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82".split('\n').toList
 
     "Part 1 example" in {
@@ -18,7 +20,7 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
     }
 
     "Part 1" in {
-      Day1Part1.getAns(input) shouldEqual 1034
+      Day1Part1.getAns(input) shouldEqual answers((day, "part1"))
     }
 
     "Part 2 example" in {
@@ -26,16 +28,17 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
     }
 
     "Part 2" in {
-      Day1Part2.getAns(input) shouldEqual 6166
+      Day1Part2.getAns(input) shouldEqual answers((day, "part2"))
     }
 
     "Part 2 optimised" in {
-      Day1Part2_optimised.getAns(input) shouldEqual 6166
+      Day1Part2_optimised.getAns(input) shouldEqual answers((day, "part2"))
     }
   }
 
   "Day 2" should {
-    val input = Utils.getInputFromDay("day2").flatMap(_.split(',').toList)
+    val day = "day2"
+    val input = Utils.getInputFromDay(day).flatMap(_.split(',').toList)
     val exampleInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124".split(',').toList
 
     "Part 1 example" in {
@@ -43,7 +46,7 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
     }
 
     "Part 1" in {
-      Day2Part1.getAns(input) shouldEqual 31000881061L
+      Day2Part1.getAns(input) shouldEqual answers((day, "part1"))
     }
 
     "Part 2 example" in {
@@ -51,12 +54,13 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
     }
 
     "Part 2" in {
-      Day2Part2.getAns(input) shouldEqual 46769308485L
+      Day2Part2.getAns(input) shouldEqual answers((day, "part2"))
     }
   }
 
   "Day 3" should {
-    val input = Utils.getInputFromDay("day3").flatMap(_.split(',').toList)
+    val day = "day3"
+    val input = Utils.getInputFromDay(day).flatMap(_.split(',').toList)
     val exampleInput = "987654321111111\n811111111111119\n234234234234278\n818181911112111".split('\n').toList
 
     "Part 1 example" in {
@@ -64,7 +68,7 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
     }
 
     "Part 1" in {
-      Day3Part1.getAns(input) shouldEqual 17432
+      Day3Part1.getAns(input) shouldEqual answers((day, "part1"))
     }
 
     "Part 2 example" in {
@@ -72,7 +76,7 @@ class PuzzlesTest extends AnyWordSpec with Matchers {
     }
 
     "Part 2" in {
-      Day3Part2.getAns(input) shouldEqual 173065202451341L
+      Day3Part2.getAns(input) shouldEqual answers((day, "part2"))
     }
   }
 }
