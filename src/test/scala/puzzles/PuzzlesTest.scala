7391cd70f90798e67e1ba0e2de0046d2554ac22b
Raymond Cheung <raymondcheung8@hotmail.com>
2025-12-16 00:57:10 +0000

    Refactor tests

--- src/test/scala/puzzles/PuzzlesTest.scala
+++ src/test/scala/puzzles/PuzzlesTest.scala
@@ -1,44 +1,48 @@
 package puzzles
 
-import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 import puzzles.day1.{Day1Part1, Day1Part2, Day1Part2_optimised}
 import puzzles.day2.{Day2Part1, Day2Part2}
 
-class PuzzlesTest extends AnyFunSuite with Matchers {
+class PuzzlesTest extends AnyWordSpec with Matchers {
 
-  test("Day1Part1") {
-    val input  = Utils.getInputFromDay("day1")
-    val actual = Day1Part1.getAns(input)
+  "Day 1" should {
+    "Part 1" in {
+      val input = Utils.getInputFromDay("day1")
+      val actual = Day1Part1.getAns(input)
 
-    actual should ===(1034)
+      actual should ===(1034)
+    }
+
+    "Part 2" in {
+      val input = Utils.getInputFromDay("day1")
+      val actual = Day1Part2.getAns(input)
+
+      actual should ===(6166)
+    }
+
+    "Part 2 optimised" in {
+      val input = Utils.getInputFromDay("day1")
+      val actual = Day1Part2_optimised.getAns(input)
+
+      actual should ===(6166)
+    }
   }
 
-  test("Day1Part2") {
-    val input  = Utils.getInputFromDay("day1")
-    val actual = Day1Part2.getAns(input)
+  "Day 2" should {
+    "Part 1" in {
+      val input = Utils.getInputFromDay("day2")
+      val actual = Day2Part1.getAns(input.head.split(',').toList)
 
-    actual should ===(6166)
-  }
+      actual should ===(31000881061L)
+    }
 
-  test("Day1Part2_optimised") {
-    val input  = Utils.getInputFromDay("day1")
-    val actual = Day1Part2_optimised.getAns(input)
+    "Part 2" in {
+      val input = Utils.getInputFromDay("day2")
+      val actual = Day2Part2.getAns(input.head.split(',').toList)
 
-    actual should ===(6166)
-  }
-
-  test("Day2Part1") {
-    val input  = Utils.getInputFromDay("day2")
-    val actual = Day2Part1.getAns(input.head.split(',').toList)
-
-    actual should ===(31000881061L)
-  }
-
-  test("Day2Part2") {
-    val input  = Utils.getInputFromDay("day2")
-    val actual = Day2Part2.getAns(input.head.split(',').toList)
-
-    actual should ===(46769308485L)
+      actual should ===(46769308485L)
+    }
   }
 }
