aa9993272775f068cbf87cb8ac1b2462ad670158
Raymond Cheung <raymondcheung8@hotmail.com>
2025-12-18 16:50:50 +0000

    Implement day7 part1

--- src/main/scala/puzzles/day7/Day7Part1.scala
+++ src/main/scala/puzzles/day7/Day7Part1.scala
@@ -1,7 +1,13 @@
 package puzzles.day7
 
 object Day7Part1 {
-  def getAns(input: List[String]): Long = {
-    ???
+  def getAns(input: List[String]): Int = {
+    val startIndex = input.head.indexOf('S')
+    input.tail.foldLeft((0, Set(startIndex))) { case ((count, indices), row) =>
+      indices.foldLeft((count, indices)) { case ((newCount, newIndices), i) =>
+        if (row(i) == '^') (newCount + 1, newIndices - i + (i - 1) + (i + 1))
+        else (newCount, newIndices)
+      }
+    }._1
   }
 }
