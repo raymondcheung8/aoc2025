2688a22fc975dfe96ad82fa04808beb2a63603a0
Raymond Cheung <raymondcheung8@hotmail.com>
2025-12-25 18:48:50 +0000

    Fix area calc bug in day9 part1

--- src/main/scala/puzzles/day9/Day9Part1.scala
+++ src/main/scala/puzzles/day9/Day9Part1.scala
@@ -2,7 +2,7 @@ package puzzles.day9
 
 object Day9Part1 {
   private def area(pair: List[(Long, Long)]): Long = pair match {
-    case List((x, y), (x2, y2), _*) => math.abs(x - x2 + 1) * math.abs(y - y2 + 1)
+    case List((x, y), (x2, y2), _*) => (math.abs(x - x2) + 1) * (math.abs(y - y2) + 1)
   }
 
   def getAns(input: List[String]): Long = {
