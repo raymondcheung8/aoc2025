ee0154ca73fc2a7129b7c772094bebb073a6b758
Raymond Cheung <raymondcheung8@hotmail.com>
2025-12-30 12:14:47 +0000

    Implement hybrid sort for day10 part2 (very slow)

--- src/main/scala/puzzles/day10/Day10Part2.scala
+++ src/main/scala/puzzles/day10/Day10Part2.scala
@@ -4,38 +4,40 @@ import scala.annotation.tailrec
 import scala.math.Ordering.Implicits.seqOrdering
 
 object Day10Part2 {
-  private case class State(state: Map[Int, (Int, Int)])
-
-  private def getNewState(state: Map[Int, (Int, Int)], button: Set[Int]): Option[State] = {
+  private def getNewState(state: Map[Int, (Int, Int)], button: Set[Int]): Option[Map[Int, (Int, Int)]] = {
     state
       .foldLeft(Option(Map.empty[Int, (Int, Int)])) {
         case (Some(acc), (i, (joltage, joltageNeeded))) if button(i) && joltageNeeded > 0   => Some(acc + (i -> (joltage + 1, joltageNeeded - 1)))
         case (Some(acc), (i, (joltage, joltageNeeded))) if !button(i) && joltageNeeded >= 0 => Some(acc + (i -> (joltage, joltageNeeded)))
         case _                                                                              => None
       }
-      .map(newState => State(newState))
+      .map(newState => newState)
   }
 
   @tailrec
   private def getMinButtonCount(
-      targetState: State,
+      targetState: Map[Int, (Int, Int)],
       buttons: Vector[Set[Int]],
-      queue: Vector[(State, Int)],
-      foundStates: Set[State] = Set()
+      queue: Vector[(Map[Int, (Int, Int)], Int, Int)],
+      foundStates: Set[Map[Int, (Int, Int)]] = Set()
   ): Option[Int] = {
-    val (State(state), count) = queue.head
-    val newStates             = buttons.flatMap(getNewState(state, _)).toSet
-    lazy val diffStates       = newStates diff foundStates
-    lazy val allFoundStates   = foundStates union newStates
-//    println(s"$totalJoltage(${targetState.totalJoltage}) --- $state")
-    println(s"${state.map(_._2._2).max} --- $state")
+    val (state, totalJoltageNeeded, count) = queue.head
+    val newStates                          = buttons.flatMap(getNewState(state, _)).toSet
+    lazy val diffStates                    = newStates diff foundStates
+    lazy val allFoundStates                = foundStates union newStates
+//    println(s"${state.map(_._2._2).max} --- $state")
 
     if (newStates(targetState)) {
-      println(s"0 --- ${targetState.state}")
+//      println(s"0 --- ${targetState}")
       Some(count + 1)
     } else {
+      val threshold            = 14
+      val greedyQueue          = (queue.tail ++ diffStates.map(newState => (newState, newState.map(_._2._2).sum, count + 1))).sortBy(_._2)
+      val greedyHeadRemainders = greedyQueue.tail.head._1.map(_._2._2)
       // We want to sort the list by the lexicographic minimum of the light joltages in descending order
-      val sortedQueue = (queue.tail ++ diffStates.map((_, count + 1))).sortBy(_._1.state.map(_._2._2).toList.sortBy(-_))
+      val sortedQueue =
+        if (greedyHeadRemainders.max - greedyHeadRemainders.min < threshold) greedyQueue
+        else (queue.tail ++ diffStates.map(state => (state, state.map(_._2._2).sum, count + 1))).sortBy(_._1.map(_._2._2).toList.sortBy(-_))
       getMinButtonCount(targetState, buttons, sortedQueue, allFoundStates)
     }
   }
@@ -51,8 +53,8 @@ object Day10Part2 {
     })
     machines.map { case (lights, buttons, joltages) =>
       println(s"starting machine...")
-      val startingState = Vector((State(joltages.view.mapValues((0, _)).toMap), 0))
-      val targetState   = State(joltages.view.mapValues((_, 0)).toMap)
+      val startingState = Vector((joltages.view.mapValues((0, _)).toMap, joltages.values.sum, 0))
+      val targetState   = joltages.view.mapValues((_, 0)).toMap
       getMinButtonCount(targetState, buttons, startingState).get
     }.sum
   }
