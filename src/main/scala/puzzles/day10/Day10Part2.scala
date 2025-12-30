75a9ec19c386943476e487e30aa634500856aba9
Raymond Cheung <raymondcheung8@hotmail.com>
2025-12-30 11:30:47 +0000

    Sort lists by lexicographic min of light joltages in day10 part2 (very slow)

--- src/main/scala/puzzles/day10/Day10Part2.scala
+++ src/main/scala/puzzles/day10/Day10Part2.scala
@@ -1,18 +1,19 @@
 package puzzles.day10
 
 import scala.annotation.tailrec
+import scala.math.Ordering.Implicits.seqOrdering
 
 object Day10Part2 {
-  private case class State(state: Map[Int, Int], totalJoltage: Int)
+  private case class State(state: Map[Int, (Int, Int)])
 
-  private def getNewState(state: Map[Int, Int], button: Set[Int], targetState: Map[Int, Int]): Option[State] = {
+  private def getNewState(state: Map[Int, (Int, Int)], button: Set[Int]): Option[State] = {
     state
-      .foldLeft(Option(Map.empty[Int, Int])) {
-        case (Some(acc), (i, joltage)) if button(i) && joltage < targetState(i)   => Some(acc + (i -> (joltage + 1)))
-        case (Some(acc), (i, joltage)) if !button(i) && joltage <= targetState(i) => Some(acc + (i -> joltage))
-        case _                                                                    => None
+      .foldLeft(Option(Map.empty[Int, (Int, Int)])) {
+        case (Some(acc), (i, (joltage, joltageNeeded))) if button(i) && joltageNeeded > 0   => Some(acc + (i -> (joltage + 1, joltageNeeded - 1)))
+        case (Some(acc), (i, (joltage, joltageNeeded))) if !button(i) && joltageNeeded >= 0 => Some(acc + (i -> (joltage, joltageNeeded)))
+        case _                                                                              => None
       }
-      .map(newState => State(newState, newState.values.sum))
+      .map(newState => State(newState))
   }
 
   @tailrec
@@ -22,13 +23,21 @@ object Day10Part2 {
       queue: Vector[(State, Int)],
       foundStates: Set[State] = Set()
   ): Option[Int] = {
-    val (State(state, totalJoltage), count) = queue.head
-    val newStates                           = buttons.flatMap(getNewState(state, _, targetState.state)).toSet
-    lazy val diffStates                     = newStates diff foundStates
-    lazy val allFoundStates                 = foundStates union newStates
+    val (State(state), count) = queue.head
+    val newStates             = buttons.flatMap(getNewState(state, _)).toSet
+    lazy val diffStates       = newStates diff foundStates
+    lazy val allFoundStates   = foundStates union newStates
+//    println(s"$totalJoltage(${targetState.totalJoltage}) --- $state")
+    println(s"${state.map(_._2._2).max} --- $state")
 
-    if (newStates(targetState)) Some(count + 1)
-    else getMinButtonCount(targetState, buttons, (queue.tail ++ diffStates.map((_, count + 1))).sortBy(-_._1.totalJoltage), allFoundStates)
+    if (newStates(targetState)) {
+      println(s"0 --- ${targetState.state}")
+      Some(count + 1)
+    } else {
+      // We want to sort the list by the lexicographic minimum of the light joltages in descending order
+      val sortedQueue = (queue.tail ++ diffStates.map((_, count + 1))).sortBy(_._1.state.map(_._2._2).toList.sortBy(-_))
+      getMinButtonCount(targetState, buttons, sortedQueue, allFoundStates)
+    }
   }
 
   def getAns(input: List[String]): Int = {
@@ -41,8 +50,9 @@ object Day10Part2 {
         )
     })
     machines.map { case (lights, buttons, joltages) =>
-      val startingState = Vector((State(joltages.view.mapValues(_ => 0).toMap, 0), 0))
-      val targetState   = State(joltages, joltages.values.sum)
+      println(s"starting machine...")
+      val startingState = Vector((State(joltages.view.mapValues((0, _)).toMap), 0))
+      val targetState   = State(joltages.view.mapValues((_, 0)).toMap)
       getMinButtonCount(targetState, buttons, startingState).get
     }.sum
   }
